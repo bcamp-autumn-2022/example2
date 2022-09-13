85f84c0b44a4268aaf426db8b530cef15d812d99
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-13 15:35:42 +0300

    firtname section works

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int age;
+string fname;
+Console.WriteLine("what is your firstname?");
+fname=Console.ReadLine();
+Console.WriteLine("Hello "+fname);
