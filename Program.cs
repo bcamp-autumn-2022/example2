f71c2c5a7f61423db06ef0a40199b167647fac90
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-13 15:48:25 +0300

    age system works

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int age;
 string fname;
+int b_year;
 Console.WriteLine("what is your firstname?");
 fname=Console.ReadLine();
 Console.WriteLine("Hello "+fname);
+Console.WriteLine("How old are you?");
+//age=int.Parse(Console.ReadLine());
+int.TryParse(Console.ReadLine(), out age);
+b_year=2022-age;
+Console.WriteLine("So you have born in "+b_year);
+
