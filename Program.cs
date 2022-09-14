7c6e53f533dd8cd538fd5f99c83137c89881d116
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-14 09:24:42 +0300

    merge main made

--- Program.cs
+++ Program.cs
@@ -2,11 +2,7 @@
 int age;
 string fname;
 int b_year;
-<<<<<<< HEAD
-Console.WriteLine("What is your Firstname?");
-=======
 Console.WriteLine("Tell me your Firstname?");
->>>>>>> main
 fname=Console.ReadLine();
 Console.WriteLine("Hello "+fname);
 Console.WriteLine("How old are you?");
